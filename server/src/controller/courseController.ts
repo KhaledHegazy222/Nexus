bcd973d6dfe4781138a0bbb97af8b24181cfa354
Omar622 <omaramin622@gmail.com>
2023-06-30 21:44:36 +0300

    Change request format of course
    * content
    * what_you_will_learn
    * requirements

--- server/src/controller/courseController.ts
+++ server/src/controller/courseController.ts
@@ -14,32 +14,24 @@ export const courseCreatePost = [
     .optional({ checkFalsy: true })
     .isNumeric()
     .withMessage('price must be a decimal'),
-  body('what_you_will_learn.*.order')
+  body('what_you_will_learn.body.*.order')
     .trim()
     .isInt()
-    .withMessage('order must be an integer number'),
-  body('requirements').custom((value: any) => {
-    if (value == null) throw new Error('requirements must be specified.')
-    const keys = Object.keys(value)
-    const nonIntegerKeys = keys.filter((key) => !/^[0-9]+$/.test(key))
-    if (nonIntegerKeys.length > 0) throw new Error('keys must be integers.')
-
-    const values: string[] = Object.values(value)
-    const emptyValues = values.filter((value) => value.length === 0)
-    if (emptyValues.length > 0) throw new Error('values must be specified.')
-    return true
-  }),
-  body('what_you_will_learn').custom((value: any) => {
-    if (value == null) throw new Error('what_you_will_learn must be specified.')
-    const keys = Object.keys(value)
-    const nonIntegerKeys = keys.filter((key) => !/^[0-9]+$/.test(key))
-    if (nonIntegerKeys.length > 0) throw new Error('keys must be integers.')
-
-    const values: string[] = Object.values(value)
-    const emptyValues = values.filter((value) => value.length === 0)
-    if (emptyValues.length > 0) throw new Error('values must be specified.')
-    return true
-  }),
+    .withMessage('order must be an integer'),
+  body('what_you_will_learn.body.*.content')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('content must be specified'),
+  body('requirements.body.*.order')
+    .trim()
+    .isInt()
+    .withMessage('order must be an integer'),
+  body('requirements.body.*.content')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('content must be specified'),
   authHelper.authenticateToken,
   roleHelper.getRole,
   async (_req: Request, _res: Response) => {
@@ -70,22 +62,16 @@ export const courseCreatePost = [
 ]
 
 export const courseEditContentPatch = [
-  body('content').custom((value: any) => {
-    if (value == null) throw new Error('content must be specified.')
-    const keys = Object.keys(value)
-    const nonIntegerKeys = keys.filter((key) => !/^[0-9]+$/.test(key))
-    if (nonIntegerKeys.length > 0) throw new Error('keys must be integers.')
-    return true
-  }),
-  body('content.*.id').not().isEmpty().withMessage('id must be specified.'),
-  body('content.*.title')
+  body('fields.*.id').not().isEmpty().withMessage('id must be specified.'),
+  body('fields.*.title')
     .not()
     .isEmpty()
     .withMessage('title must be specified.'),
-  body('content.*.type')
+  body('fields.*.type')
     .isIn(['video', 'reading', 'quiz'])
     .withMessage('invalid value.'),
-  body('content.*.public').isBoolean().withMessage('public must be boolean.'),
+  body('fields.*.public').isBoolean().withMessage('public must be boolean.'),
+  body('fields.*.order').trim().isInt().withMessage('order must be integer.'),
   authHelper.authenticateToken,
   async (_req: Request, _res: Response) => {
     const errors: Result<ValidationError> = validationResult(_req)
@@ -109,20 +95,24 @@ export const courseEditContentPatch = [
         queries.queryList.GET_COURSE_CONTENT,
         [courseId]
       )
-      const oldContent = queryResp2.rows[0].content
-      const oldIds = Object.values(oldContent).map((value: any) => value.id)
+      if (queryResp2.rows[0].content !== null) {
+        const oldFields: object[] = queryResp2.rows[0].content.fields
+        const oldIds: string[] = oldFields.map((value: any) => value.id)
 
-      const newContent = _req.body.content
-      const newIds = Object.values(newContent).map((value: any) => value.id)
+        const newFields = _req.body.fields
+        const newIds: string[] = newFields.map((value: any) => value.id)
 
-      const toDelete = oldIds.filter((id: string) => !newIds.includes(id))
-      console.log(toDelete)
+        const toDelete: string[] = oldIds.filter(
+          (id: string) => !newIds.includes(id)
+        )
+        console.log('delete from s3 ', toDelete)
 
-      // TODO: delete toDelete files from s3
+        // TODO: delete toDelete files from s3
+      }
 
       // update course content
       await dbConnection.dbQuery(queries.queryList.UPDATE_COURSE_CONTENT, [
-        _req.body.content,
+        _req.body,
         courseId
       ])
 
@@ -135,13 +125,13 @@ export const courseEditContentPatch = [
 
 export const courseDetailsGet = [
   authHelper.authenticateToken,
-  roleHelper.checkCourseFullAccess,
   async (_req: Request, _res: Response) => {
     try {
       const queryResp1 = await dbConnection.dbQuery(
         queries.queryList.GET_COURSE,
         [_req.params.courseId]
       )
+      if (queryResp1.rows.length === 0) return _res.sendStatus(400)
       const course = queryResp1.rows[0]
 
       const queryResp2 = await dbConnection.dbQuery(
@@ -151,17 +141,6 @@ export const courseDetailsGet = [
       const author = queryResp2.rows[0]
 
       course.author = author
-      if (_res.locals.hasFullAccess === true) {
-        return _res.status(200).json(course)
-      }
-
-      const publicCourseContent: Record<string, any> = {}
-      Object.keys(course.content).forEach((key: string) => {
-        if (course.content[key].public === 'true') {
-          publicCourseContent[key] = course.content[key]
-        }
-      })
-      course.content = publicCourseContent
 
       return _res.status(200).json(course)
     } catch {
