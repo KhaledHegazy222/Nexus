e22521a5e40c99c8913d6a45dbabf6d9ce37c192
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-07-08 17:39:32 +0300

    Handle empty videos before uploading

--- client/src/components/instructor/Lesson/Video.tsx
+++ client/src/components/instructor/Lesson/Video.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import useAuth from "@/contexts/useAuth";
 import { serverAxios } from "@/utils/axios";
-import { Button, FormControl } from "@mui/material";
-import { Dialog, DialogContent, Input, Typography } from "@mui/material";
-import axios from "axios";
-import { resolveNaptr, resolveNs } from "dns";
-import React, { useEffect, useRef, useState } from "react";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
+
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Video = () => {
   const { courseId, lessonId } = useParams();
   const { token } = useAuth();
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  useEffect(() => {
-    loadData();
-    async function loadData() {
+  const [videoStreamUrl, setVideoStreamUrl] = useState<string | null>(null);
+
+  const [loadFetching, setLoadFetching] = useState(true);
+  const [loadUploading, setLoadUploading] = useState(false);
+  const fetchData = useCallback(async () => {
+    setLoadFetching(true);
+    try {
       const response = await serverAxios.get(
         `/course/${courseId}/video/${lessonId}`,
         {
@@ -22,45 +23,99 @@ const Video = () => {
         }
       );
       const { token: videoToken } = response.data;
-      if (videoRef.current) {
-        videoRef.current.src = `${
+      setVideoStreamUrl(
+        `${
           import.meta.env.VITE_API_ROOT_URL
-        }/api/v1/course/video/${lessonId}/${videoToken}`;
-      }
+        }/api/v1/course/video/${lessonId}/${videoToken}`
+      );
+    } catch {
+      setLoadFetching(false);
     }
+    setLoadFetching(false);
   }, [token, courseId, lessonId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   const handleFileUpload: React.ChangeEventHandler<HTMLInputElement> = async (
     e
   ) => {
     if (e.target.files) {
-      const requestBody = new FormData();
-      requestBody.append("file", e.target.files[0]);
-      await serverAxios.post(
-        `/course/${courseId}/video/upload/${lessonId}`,
-        requestBody,
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
+      setLoadFetching(true);
+      try {
+        const requestBody = new FormData();
+        requestBody.append("file", e.target.files[0]);
+        await serverAxios.post(
+          `/course/${courseId}/video/upload/${lessonId}`,
+          requestBody,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+      setLoadFetching(false);
+      fetchData();
     }
   };
 
   return (
     <>
-      <Button variant="contained" component="label">
-        Upload
-        <input
-          hidden
-          accept="video/*"
-          type="file"
-          onChange={handleFileUpload}
-        />
-      </Button>
-      <video ref={videoRef} width="500" height="600" controls>
-        <source
-          src="blob:http://localhost:3000/b652e450-d927-4ef9-a9cc-bd189f139409"
-          type="video/mp4"
-        ></source>
-      </video>
+      {loadFetching ? (
+        <>
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CircularProgress color="primary" size={60} />
+            <Typography variant="h6">Loading your video.... </Typography>
+          </Box>
+        </>
+      ) : (
+        <>
+          {videoStreamUrl === null ? (
+            <>
+              <Typography>
+                {"Sorry But it seems like you didn't upload any video yet"}
+              </Typography>
+            </>
+          ) : (
+            <>
+              <video
+                width="500"
+                height="600"
+                controls
+                src={videoStreamUrl}
+              ></video>
+            </>
+          )}
+          <Button
+            variant="contained"
+            component="label"
+            sx={{
+              m: "20px",
+            }}
+          >
+            Upload Video
+            <input
+              hidden
+              accept="video/*"
+              type="file"
+              onChange={handleFileUpload}
+            />
+          </Button>
+        </>
+      )}
     </>
   );
 };
