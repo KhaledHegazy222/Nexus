bafed423cd0e2efc26721176444d40b0788be604
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-07-24 22:51:03 +0300

    ِAdd Structure for quiz and reading pages

--- client/src/pages/Student/StudyPage/Lesson/Quiz.tsx
+++ client/src/pages/Student/StudyPage/Lesson/Quiz.tsx
@@ -1,5 +1,127 @@
+import React, { useState } from "react";
+import LessonButtons from "./LessonButtons";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  List,
+  Paper,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+
+const quizQuestions = [
+  {
+    title: "question title1",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title2",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title2",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title3",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title4",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title5",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+  {
+    title: "question title6",
+    options: {
+      content: ["option1", "option2", "option3", "option4"],
+    },
+  },
+];
+
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [completed, setCompleted] = useState(false);
+  const toggleCompleted = () => {
+    setCompleted(!completed);
+  };
+  return (
+    <>
+      <Paper
+        sx={{
+          margin: "10px",
+          marginBottom: "30px",
+          padding: "10px",
+          height: "480px",
+          overflow: "auto",
+        }}
+      >
+        <List>
+          {quizQuestions.map((question) => (
+            <React.Fragment key={question.title}>
+              <Typography
+                variant="h5"
+                sx={{
+                  textTransform: "capitalize",
+                }}
+              >
+                {question.title}
+              </Typography>
+              <FormControl
+                sx={{
+                  padding: "10px 30px",
+                }}
+              >
+                <RadioGroup>
+                  {question.options.content.map((option) => (
+                    <FormControlLabel
+                      key={option}
+                      value={option}
+                      control={
+                        <Radio
+                          sx={{
+                            color: "primary.main",
+                          }}
+                        />
+                      }
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </React.Fragment>
+          ))}
+        </List>
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{
+            display: "block",
+            margin: "20px auto",
+          }}
+        >
+          Submit
+        </Button>
+      </Paper>
+      <LessonButtons completed={completed} toggleCompleted={toggleCompleted} />
+    </>
+  );
 };
 
 export default Quiz;
--- client/src/pages/Student/StudyPage/Lesson/Reading.tsx
+++ client/src/pages/Student/StudyPage/Lesson/Reading.tsx
@@ -1,5 +1,33 @@
+import { Paper } from "@mui/material";
+import { useState } from "react";
+import LessonButtons from "./LessonButtons";
+
+const textInitialValue: string =
+  '<h1>Lesson Title</h1><h2><strong>Lesson Subtitle </strong></h2><p>Lesson paragraph</p><p><br></p><h3>Main topics</h3><ol><li>Topic one</li><li>Topic two</li><li>Topic three</li><li>Topic four</li></ol><h3><strong>Reference:</strong></h3><ul><li><a href="https://www.wikipedia.org/" rel="noopener noreferrer" target="_blank">Wikipedia</a></li><li><a href="https://www.youtube.com/" rel="noopener noreferrer" target="_blank">Video Playlist</a></li></ul>';
+
 const Reading = () => {
-  return <div>Reading</div>;
+  const [text] = useState(textInitialValue);
+
+  const [completed, setCompleted] = useState(false);
+  const toggleCompleted = () => {
+    setCompleted(!completed);
+  };
+  return (
+    <>
+      <Paper
+        dangerouslySetInnerHTML={{
+          __html: text,
+        }}
+        sx={{
+          padding: "10px",
+          margin: "10px",
+          height: "480px",
+          overflow: "auto",
+        }}
+      ></Paper>
+      <LessonButtons completed={completed} toggleCompleted={toggleCompleted} />
+    </>
+  );
 };
 
 export default Reading;
