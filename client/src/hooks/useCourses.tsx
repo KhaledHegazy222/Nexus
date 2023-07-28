a52822f4a478bfa7fc7ba82bd0b39c737d630422
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-07-28 16:35:37 +0300

    Fix Next and Previous Buttons

--- client/src/hooks/useCourses.tsx
+++ client/src/hooks/useCourses.tsx
@@ -1,91 +1,42 @@
 import { useEffect, useState } from "react";
 import courseImage from "@/assets/images/course.jpg";
-export type courseType = {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-};
-
-const coursesSample: courseType[] = [
-  {
-    id: 1,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 2,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 3,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 4,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 5,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 6,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 7,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 8,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 9,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 10,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-  {
-    id: 11,
-    title: "Learn Python in 3 minutes",
-    description: "Learn Learn Learn Learn Learn Learn Learn Learn Learn",
-    image: courseImage,
-  },
-];
+import { serverAxios } from "@/utils/axios";
+import useAuth from "@/contexts/useAuth";
+export type courseType = Course;
 
 const useCourses = () => {
+  const { token } = useAuth();
   const [loading, setLoading] = useState<boolean>(true);
   const [courses, setCourses] = useState<courseType[]>([]);
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setCourses(coursesSample);
+    fetchData();
+    async function fetchData() {
+      setLoading(true);
+      const response = await serverAxios.get(`/course`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCourses(
+        response.data.map(
+          (elem: {
+            id: number;
+            title: string;
+            first_name: string;
+            last_name: string;
+            completion_percentage?: string;
+          }): courseType => ({
+            id: elem.id,
+            description: "Learn Learn Learn",
+            image: courseImage,
+            title: elem.title,
+            progress: elem.completion_percentage,
+          })
+        )
+      );
       setLoading(false);
-    }, 1000);
-  }, []);
+    }
+  }, [token]);
 
   return { loading, courses };
 };
