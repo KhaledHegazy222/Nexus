7abc7ff8d95625ed114b66edd92085528b5a0d92
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-08-08 00:25:05 +0300

    Add Spinner in loading Reading Lessons

--- client/src/pages/Student/StudyPage/Lesson/Reading.tsx
+++ client/src/pages/Student/StudyPage/Lesson/Reading.tsx
@@ -1,30 +1,64 @@
-import { Paper } from "@mui/material";
-import { useState } from "react";
+import { Box, CircularProgress, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
 import LessonButtons from "./LessonButtons";
+import { useParams } from "react-router-dom";
+import { serverAxios } from "@/utils/axios";
+import useAuth from "@/contexts/useAuth";
 
 const textInitialValue: string =
   '<h1>Lesson Title</h1><h2><strong>Lesson Subtitle </strong></h2><p>Lesson paragraph</p><p><br></p><h3>Main topics</h3><ol><li>Topic one</li><li>Topic two</li><li>Topic three</li><li>Topic four</li></ol><h3><strong>Reference:</strong></h3><ul><li><a href="https://www.wikipedia.org/" rel="noopener noreferrer" target="_blank">Wikipedia</a></li><li><a href="https://www.youtube.com/" rel="noopener noreferrer" target="_blank">Video Playlist</a></li></ul>';
 
 const Reading = () => {
-  const [text] = useState(textInitialValue);
-
+  const { lessonId } = useParams();
+  const { token } = useAuth();
+  const [text, setText] = useState(textInitialValue);
+  const [loading, setLoading] = useState(true);
   const [completed, setCompleted] = useState(false);
   const toggleCompleted = () => {
     setCompleted(!completed);
   };
+  useEffect(() => {
+    loadData();
+    async function loadData() {
+      try {
+        setLoading(true);
+        const response = await serverAxios.get(`/lesson/reading/${lessonId}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setText(response.data.content);
+      } catch {
+        /* empty */
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, [token, lessonId]);
   return (
     <>
-      <Paper
-        dangerouslySetInnerHTML={{
-          __html: text,
-        }}
-        sx={{
-          padding: "10px",
-          margin: "10px",
-          height: "480px",
-          overflow: "auto",
-        }}
-      ></Paper>
+      {loading ? (
+        <Box
+          sx={{
+            height: "480px",
+            display: "grid",
+            placeContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Paper
+          dangerouslySetInnerHTML={{
+            __html: text,
+          }}
+          sx={{
+            padding: "10px",
+            margin: "10px",
+            height: "480px",
+            overflow: "auto",
+          }}
+        ></Paper>
+      )}
+
       <LessonButtons completed={completed} toggleCompleted={toggleCompleted} />
     </>
   );
