ca1f3f22e7ffb7155f0a12999f2a95383831d317
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-08-07 17:39:31 +0300

    Refactor Create course page

--- client/src/components/instructor/CreateCourse/GeneralInfo.tsx
+++ client/src/components/instructor/CreateCourse/GeneralInfo.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import TextField from "./TextField";
 import { UseFormRegister } from "react-hook-form";
 import { FormValuesType } from "./CreateCourse";
 import SelectField from "./SelectField";
+import { StyledGridCell } from "./CreateCourse.styled";
 
 export type GeneralInfoType = {
   title: string;
@@ -27,35 +28,70 @@ const GeneralInfo: FC<GeneralInfoProps> = ({ register }) => {
     >
       <Typography variant="h4">Course Info.</Typography>
       <hr />
-      <TextField register={register} name="title" label="Title" type="text" />
-      <TextField
-        register={register}
-        name="description"
-        label="Description"
-        type="text"
-        multiline={true}
-        maxRows={5}
-      />
-      <TextField
-        register={register}
-        name="price"
-        label="Price"
-        type="number"
-        step={0.01}
-        helperText="Enter the price in EGP"
-      />
-      <SelectField
-        register={register}
-        name="level"
-        title="Level"
-        options={["Beginner", "Intermediate", "Advanced"]}
-      />
-      <SelectField
-        register={register}
-        name="field"
-        title="Field"
-        options={["Software", "Hardware", "Mechanical"]}
-      />
+      <Grid container sx={{ m: "0 auto", p: "0" }}>
+        <StyledGridCell item xs={6}>
+          <TextField
+            register={register}
+            name="title"
+            label="Title"
+            type="text"
+            sx={{
+              maxWidth: "300px",
+            }}
+          />
+        </StyledGridCell>
+        <StyledGridCell item xs={6} sx={{}}>
+          <TextField
+            register={register}
+            name="price"
+            label="Price in EGP"
+            type="number"
+            step={0.01}
+            fullWidth
+            sx={{
+              maxWidth: "300px",
+              marginLeft: "auto",
+              display: "block",
+            }}
+          />
+        </StyledGridCell>
+        <StyledGridCell item xs={12}>
+          <TextField
+            register={register}
+            name="description"
+            label="Description"
+            type="text"
+            multiline={true}
+            rows={4}
+            sx={{
+              width: "100%",
+            }}
+          />
+        </StyledGridCell>
+        <StyledGridCell item xs={6}>
+          <SelectField
+            register={register}
+            name="level"
+            title="Level"
+            options={["Beginner", "Intermediate", "Advanced"]}
+            sx={{
+              maxWidth: "300px",
+            }}
+          />
+        </StyledGridCell>
+        <StyledGridCell item xs={6}>
+          <SelectField
+            register={register}
+            name="field"
+            title="Field"
+            options={["Software", "Hardware", "Mechanical"]}
+            sx={{
+              maxWidth: "300px",
+              marginLeft: "auto",
+            }}
+          />
+        </StyledGridCell>
+      </Grid>
     </Box>
   );
 };
