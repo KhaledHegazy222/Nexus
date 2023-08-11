669226239f94f4e62f3a3087e5298b82032f7570
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-08-11 03:49:04 +0300

    Add Missing Courses Message

--- client/src/pages/Student/MyCourses/index.tsx
+++ client/src/pages/Student/MyCourses/index.tsx
@@ -1,11 +1,14 @@
 import CourseCard from "@/components/CourseCard";
 import Skeleton from "@/components/Skeleton";
 import useCourses from "@/hooks/useCourses";
-import { Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import noCourses from "@/assets/images/nocourses.jpg";
 
 const MyCourses = () => {
   const skeletonItems = Array(10).fill(null);
   const { loading, courses } = useCourses();
+  const navigate = useNavigate();
   return (
     <>
       <Typography
@@ -39,25 +42,52 @@ const MyCourses = () => {
           ))
         ) : (
           <>
-            {" "}
-            {courses.map((course) => (
-              <Grid
-                item
-                xs={"auto"}
-                key={course.id}
-                style={{
-                  width: "clamp(340px, 33%,450px)",
-                  padding: "10px",
+            {courses.length ? (
+              courses.map((course) => (
+                <Grid
+                  item
+                  xs={"auto"}
+                  key={course.id}
+                  style={{
+                    width: "clamp(340px, 33%,450px)",
+                    padding: "10px",
+                  }}
+                >
+                  <CourseCard
+                    link={`/student/course/${course.id}`}
+                    image={course.image!}
+                    title={course.title}
+                    progress={course.progress}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "20px",
                 }}
               >
-                <CourseCard
-                  link={`/student/course/${course.id}`}
-                  image={course.image!}
-                  title={course.title}
-                  progress={course.progress}
+                <img
+                  src={noCourses}
+                  style={{ maxWidth: "100%", maxHeight: "50vh" }}
                 />
-              </Grid>
-            ))}
+                <Typography variant="h6">
+                  Looks like you didn&apos;t enroll to any course yet. Start
+                  Learning Now
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => navigate("/explore")}
+                >
+                  Explore Now
+                </Button>
+              </Box>
+            )}
           </>
         )}
       </Grid>
